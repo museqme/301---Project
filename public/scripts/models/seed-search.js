44c2b1bae3ee23a209468628875e3205ba6217e3
lonewalker72 <walkisai.72@gmail.com>
2017-08-03 10:48:26 -0700

    please do the thing

--- public/scripts/models/seed-search.js
+++ public/scripts/models/seed-search.js
@@ -5,18 +5,18 @@ $('#seedForm').submit(function(event) {
   var strainSearch = $('#strainSearch').val();
   console.log(strainSearch);
   // return strainSearch
-  $.getJSON('https://www.cannabisreports.com/api/v1.0/strains/search/' + strainSearch)
+  $.getJSON('strains/search/' + strainSearch)
   .then(searchRequest, errorCallback);
 });
-  let searchRequest = function(data) {
-    console.log(data)
-    var obj = data;
-    var objToArr = Object.keys(obj).map(function(key) {return obj[key]});
-    console.log(objToArr);
-    var dataArr = objToArr[0];
-    console.log(dataArr);
-    let template = Handlebars.compile($('#strainResults').html())
-    $('#strains').html(template({strain: dataArr}))
+let searchRequest = function(data) {
+  console.log(data)
+  var obj = data;
+  var objToArr = Object.keys(obj).map(function(key) {return obj[key]});
+  console.log(objToArr);
+  var dataArr = objToArr[0];
+  console.log(dataArr);
+  let template = Handlebars.compile($('#strainResults').html())
+  $('#strains').html(template({strain: dataArr}))
 };
     // let errorCallback = function(err) {
     //   console.error(err)
