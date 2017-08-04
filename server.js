57f37c2c753e7e018bef1d0e5f4d57ba8ef377a7
lonewalker72 <walkisai.72@gmail.com>
2017-08-03 17:22:54 -0700

    tweek to next prev

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function proxyCanApi(request, response) {
   console.log(request.headers)
   console.log('Routing CannabisReports request for strains!');
   (requestProxy({
-    url: `https://www.cannabisreports.com/api/v1.0/strains/${request.params[0]}`,
+    url: `https://www.cannabisreports.com/api/v1.0/strains`,
     headers: {Authorization: `${process.env.X_API_KEY}`}
   }))(request, response);
 }
@@ -42,21 +42,21 @@ function proxyCanApiSeeds(request, response) {
   }))(request, response);
 }
 
-// function proxyCanApiNext(request, response) {
-//   // console.log('Isaiah was here: proxyCanApiSeeds');
-//   // console.log(request.headers)
-//   // console.log('Routing CannabisReports request for search query!');
-//   console.log(request.params[1]);
-//   (requestProxy({
-//     url: `https://www.cannabisreports.com/api/v1.0/strains/${request.params[1]}`,
-//     headers: {Authorization: `${process.env.X_API_KEY}`}
-//   }))(request, response);
-// }
+function proxyCanApiNext(request, response) {
+  // console.log('Isaiah was here: proxyCanApiSeeds');
+  // console.log(request.headers)
+  // console.log('Routing CannabisReports request for search query!');
+  console.log(request.params[0]);
+  (requestProxy({
+    url: `https://www.cannabisreports.com/api/v1.0/strains/${request.params[0]}`,
+    headers: {Authorization: `${process.env.X_API_KEY}`}
+  }))(request, response);
+}
 
 
 app.get('/strains', proxyCanApi);
 app.get('/search/*', proxyCanApiSeeds);
-// app.get('/strains/*', proxyCanApiNext);
+app.get('/strains/*', proxyCanApiNext);
 
 app.listen(PORT, function() {
   console.log(`'listening on PORT: ${PORT}'`)
